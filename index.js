2a2c8f35ff0099cf15341773a0669ae4803402cd
Oskar Eriksson <oskar@teamtailor.com>
2022-09-06 13:35:12 +0200

    Action should work

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const playwright = require('playwright');
 
 (async () => {
   try {
-    const url = core.getInput('url');
+    const url = core.getInput('URL');
 
     const browser = await playwright.chromium.launch();
     const page = await browser.newPage();
 
-    await page.goto(`https://validator.nu/?doc=${url}`);
+    // const reportUrl = `https://validator.nu/?doc=${url}`;
+    const reportUrl =
+      'https://validator.nu/?doc=https://accessibilitydemo.teamtailor.app';
+    console.log('Navigating to', reportUrl);
+    await page.goto(reportUrl);
 
-    await page.waitForSelector('#results .error');
+    await page.waitForSelector('#results');
     const errorsCount = await page.locator('#results .error').count();
     await browser.close();
 
+    console.log('Errors count', errorsCount);
+
     core.setOutput('errors-count', errorsCount);
+    core.setOutput('report-url', reportUrl);
   } catch (error) {
     core.setFailed(error.message);
   }
